abb855999b0e44ff1a92752825d0261b45b7beae
Biwaby <456kam00@gmail.com>
2023-05-12 18:02:50 +0300

    Исправление и пушнинг сайта в репозиторий

--- scripts/docs.js
+++ scripts/docs.js
@@ -43,38 +43,12 @@ logIn_btn_footer.onclick = function() {
     });
 }
 
+// Docs formatation script
+let doc_link = document.getElementsByClassName("document-link");
 
-// Carousel script
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-    var indicators = document.getElementsByClassName("sld-ind");
-
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i  = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+var i;
+for (i = 0; i < doc_link.length; i++) {
+    if ((i + 1) % 2 != 0) {
+        doc_link[i].style = "margin-left: 50%";
     }
-    for (i = 0; i < indicators.length; i++) {
-        indicators[i].className = indicators[i].className.replace(" active", "");
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-    indicators[slideIndex - 1].className += " active";
 }
-
-setInterval (
-    () => plusSlides(1),
-    6000
-);
--- scripts/teams/team-1.js
+++ scripts/teams/team-1.js
@@ -43,38 +43,25 @@ logIn_btn_footer.onclick = function() {
     });
 }
 
+// Team formatation script
+let team_struct = document.querySelector("#team_struct");
+let match_sched = document.querySelector("#match_sched");
+let team_static = document.querySelector("#team_static");
 
-// Carousel script
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+team_struct.onclick = function() {
+    flagDiv.classList.add("openedStruct");
+    flagDiv.classList.remove("openedSched");
+    flagDiv.classList.remove("openedStatist");
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+match_sched.onclick = function() {
+    flagDiv.classList.remove("openedStruct");
+    flagDiv.classList.add("openedSched");
+    flagDiv.classList.remove("openedStatist");
 }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-    var indicators = document.getElementsByClassName("sld-ind");
-
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i  = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < indicators.length; i++) {
-        indicators[i].className = indicators[i].className.replace(" active", "");
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-    indicators[slideIndex - 1].className += " active";
+team_static.onclick = function() {
+    flagDiv.classList.remove("openedStruct");
+    flagDiv.classList.remove("openedSched");
+    flagDiv.classList.add("openedStatist");
 }
-
-setInterval (
-    () => plusSlides(1),
-    6000
-);
