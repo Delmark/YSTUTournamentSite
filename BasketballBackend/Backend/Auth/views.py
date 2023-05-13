22184a6bec476f871bac9da6e7bc09ff2a6cfc96
Delmark <102899273+Delmark@users.noreply.github.com>
2023-05-13 07:31:54 +0300

    Создана заготовка под систему регистрации Отредактирована форма, прикреплена примерная форма логики регистрации

--- BasketballBackend/Backend/Auth/views.py
+++ BasketballBackend/Backend/Auth/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        otchestvo = request.POST['otchestvo']
+        birth_date = request.POST['birth_date']
+        email = request.POST['email']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+
+            # Create user
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            otchestvo=otchestvo,
+            birth_date=birth_date,
+            email=email,
+            username=username,
+            password=password1)
+            
+        login(request, user)
+        return redirect('home')
+    return render(request, 'index.html')  
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'index.html')
+
+def logout_view(request): 
+    logout(request)
+    return redirect('home')
--- BasketballBackend/Backend/Backend/settings.py
+++ BasketballBackend/Backend/Backend/settings.py
@@ -132,8 +132,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = (BASE_DIR / 'media')
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
